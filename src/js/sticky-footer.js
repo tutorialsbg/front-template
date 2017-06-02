08b3e9b742d948702a8d76ce3aaff9a3b7cd0185
Slavi Slavchev <slavi.lns@gmail.com>
2017-06-02 17:35:20 +0300

    git init

---
+++ src/js/sticky-footer.js
@@ -0,0 +1,21 @@
+
+ function autoHeight() {
+   $('#sticky').css('min-height', 0);
+   $('#sticky').css('min-height', (
+     $(document).height() 
+     - $('#header').height() 
+     - $('#footer').height()
+     + $('#bs-example-navbar-collapse-1').height()
+
+   ));
+ }
+
+ // onDocumentReady function bind
+ $(document).ready(function() {
+   autoHeight();
+ });
+
+ // onResize bind of the function
+ $(window).resize(function() {
+   autoHeight();
+ });
